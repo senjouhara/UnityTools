b904fd5ca1978b5cde47e3c7f97e90d4430a9f5f
Chambers <astring1812@126.com>
2017-08-31 00:06:11 +0800

    创建

---
+++ Assets/AddLodGroup.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddLodGroup : MonoBehaviour
+{
+
+    private MeshRenderer[] renderers;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    [ContextMenu("AddLOD")]
+    private void AddLOD()
+    {
+        renderers = GetComponentsInChildren<MeshRenderer>();
+        LOD[] lods = new LOD[2];
+        
+        for (int i = 0; i < renderers.Length; i++)
+        {
+            var group = renderers[i].GetComponent<LODGroup>();
+            Renderer[] mr = new Renderer[1];
+            mr[0] = renderers[i];
+            if (!group)
+            {
+                group = renderers[i].gameObject.AddComponent<LODGroup>();
+            }
+            lods[0] = new LOD(0.1f, mr);
+
+            group.SetLODs(lods);
+
+        }
+
+    }
+}
